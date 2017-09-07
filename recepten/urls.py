98173924102abc578c8c2297fbdd3db686e4cb44
pieterVL <pieter.van.leuven01@gmail.com>
2017-09-07 14:24:07 +0200

    Clean old project + fix mongo

--- recepten/urls.py
+++ recepten/urls.py
@@ -3,8 +3,4 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^delete_post/(?P<topic_para>(.*))$', views.topic_post_delete, name='topic'),
-    url(r'^delete/(?P<topic_para>(.*))$', views.topic_delete, name='topic'),
-    # url(r'^(?P<topic_para>(.*))$', views.topic, name='topic'), #any character
-    url(r'^(?P<topic_para>[0-z]+)', views.topic, name='topic'),
 ]
--- recepten/views.py
+++ recepten/views.py
@@ -2,49 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
-import redis
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
+from pymongo import MongoClient
 
+client = MongoClient()
+# db = client['primer']
+# coll = db['dataset']
+db = client['Recepten']
+coll = db['test']
 # Create your views here.
 def index(request):
-	# return HttpResponse("<h2>Hey</h2>")
-	if request.method == 'GET':
-		return render(request, 'forum/index.html', {'topics':getTopics()})
-	elif request.method == 'POST':
-		subject = request.POST['subject']; 
-		addTopic(subject)
-		addPost(subject, request.POST['post'])
-		return redirect('.')
+	vari =  insert(0)
+	print vari
+	print vari.inserted_id
+	return HttpResponse("<h2>Hey</h2>")
 
-def topic(request, topic_para):
-	if request.method == 'GET':
-		return render(request, 'forum/topic.html', {'topic':topic_para, 'posts':getPosts(topic_para)})
-	elif request.method == 'POST':
-		addPost(topic_para,request.POST['post'])
-		return redirect(request.get_full_path())
+	# if request.method == 'GET':
+	# 	return render(request, 'forum/index.html', {'topics':getTopics()})
+	# elif request.method == 'POST':
+	# 	subject = request.POST['subject']; 
+	# 	addTopic(subject)
+	# 	addPost(subject, request.POST['post'])
+	# 	return redirect('.')
 
-def topic_delete(request, topic_para):	
-	if request.method == 'POST':
-		deleteTopic(topic_para)
-		return redirect('./..')
-
-def topic_post_delete(request, topic_para):
-	if request.method == 'POST':
-		deleletePost(topic_para, request.POST['key'])
-		return redirect('./../'+topic_para)
-
-
-
-def addTopic(subject):
-	r.lpush('subjects',subject)
-def addPost(subject, post):
-	r.rpush('subjects:'+subject, post)
-def getTopics():
-	return r.lrange('subjects',0,-1)
-def getPosts(subject):
-	return r.lrange('subjects:'+subject,0,-1)
-def deleteTopic(subject):
-	r.lrem('subjects',0,subject)
-	r.delete('subjects:'+subject)
-def deleletePost(subject,key):
-	r.lrem('subjects:'+subject,0,key)
+def insert(obj):
+	result = db['test'].insert_one({'key':'value'})
+	return result
